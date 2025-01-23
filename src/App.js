2e7be2df5e7a11f0c3e6f85e7dc0f84c93934774
Simeon Cargile <cargileaws@gmail.com>
2025-01-23 01:40:23 -0500

    change ubuntu version on deploy.yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I will do good on this interview!
         </p>
         <a
           className="App-link"
